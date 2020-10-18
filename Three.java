ae4a104d1eee1f92d66155f367825b081fff011d
MadhushreeP4 <72905721+MadhushreeP4@users.noreply.github.com>
2020-10-18 18:07:51 +0530

    Third commit

---
+++ Three.java
@@ -0,0 +1,17 @@
+class Info
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Madhushree");
+		music();
+		drinking();
+	}
+	public static void music()
+	{
+		System.out.println("X-workz");
+	}
+	public static void drinking()
+	{
+		System.out.println("Electronics & Communication");
+	}
+}
