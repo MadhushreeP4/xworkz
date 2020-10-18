0726576b86548213162a08b97015c7cf1904b0a3
MadhushreeP4 <72905721+MadhushreeP4@users.noreply.github.com>
2020-10-18 18:09:18 +0530

    Fourth commit

---
+++ Five.java
@@ -0,0 +1,32 @@
+class Developer
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Space Agencies");
+		india();
+		america();
+		japan();
+		russia();
+		europe();
+	}
+	public static void india()
+	{
+		System.out.println("ISRO - Indian Space Research Organization");
+	}
+	public static void america()
+	{
+		System.out.println("NASA - National Aeronautics and Space Administration");
+	}
+	public static void japan()
+	{
+		System.out.println("JAXA - Japan Aerospace Exploration Agency");
+	}
+	public static void russia()
+	{
+		System.out.println("RFSA - Russian Federation Space Agency");
+	}
+	public static void europe()
+	{
+		System.out.println("ESA - Eueopean Space Agency");
+	}
+}
