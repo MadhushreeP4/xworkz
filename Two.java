eb6abb1e6e0d7f252e535d9ae1c1a7f1ff4caae7
MadhushreeP4 <72905721+MadhushreeP4@users.noreply.github.com>
2020-10-18 17:50:57 +0530

    First commit

---
+++ Two.java
@@ -0,0 +1,14 @@
+class Invoking
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Language");
+		write();
+		write();
+		write();
+	}
+	public static void write()
+	{
+		System.out.println("JAVA");
+	}
+}
