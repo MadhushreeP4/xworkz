e9d66dd1a1163eb48534db5f21973880267ceb9d
MadhushreeP4 <72905721+MadhushreeP4@users.noreply.github.com>
2020-10-19 19:59:37 +0530

    Sixth commit

---
+++ Box.java
@@ -0,0 +1,33 @@
+public class Box{
+	public static void main(String[] args)
+	{
+		byte numberOfContinents=7;
+		
+		short squarefeetOfIsroCenter=1000;
+		
+		int numberOfCoronaCasesInIndia=7500000;
+		
+		long populationOfChina=1400000000;
+		
+		float maxUsedNokiaPhoneVersion=6.2f;
+		
+		double cubeRootOfTwoHundredAndTwentyTwo=6.05504895;
+		
+		boolean BlackHolesAreTheDarkestThingsInUniverse=true;
+		
+		char firstLetterOfOurContry='I';
+		
+		String ourGalaxyName="Milkyway";
+		
+		System.out.println(numberOfContinents);
+		System.out.println(squarefeetOfIsroCenter);
+		System.out.println(numberOfCoronaCasesInIndia);
+		System.out.println(populationOfChina);
+		System.out.println(maxUsedNokiaPhoneVersion);
+		System.out.println(cubeRootOfTwoHundredAndTwentyTwo);
+		System.out.println(BlackHolesAreTheDarkestThingsInUniverse);
+		System.out.println(firstLetterOfOurContry);
+		System.out.println(ourGalaxyName);
+	}
+	
+}
