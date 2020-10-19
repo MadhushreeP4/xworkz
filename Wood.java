a151b72096aba01547448a650092f1a2fca1da48
MadhushreeP4 <72905721+MadhushreeP4@users.noreply.github.com>
2020-10-19 20:00:38 +0530

    Seventh commit

---
+++ Wood.java
@@ -0,0 +1,31 @@
+public class Wood{
+	public static void main(String[] args)
+	{
+		byte totalNumberOfMatchesinIplThisYear=56;
+		System.out.println(totalNumberOfMatchesinIplThisYear);
+		
+		short totalNumberOfPlayersinIplThisYear=191;
+		System.out.println(totalNumberOfPlayersinIplThisYear);
+		
+		int minimumAmountSpentOnPlayer=2000000;
+		System.out.println(minimumAmountSpentOnPlayer);
+		
+		long totalAmountPerTeam=800000000;
+		System.out.println(totalAmountPerTeam);
+		
+		float avgWeightOfCricketBatInKg=1.3f;
+		System.out.println(avgWeightOfCricketBatInKg);
+		
+		double weightOfCricketBallInGrams=163.009758;
+		System.out.println(weightOfCricketBallInGrams);
+		
+		boolean eightTeamsAreThereInIpl=true;
+		System.out.println(eightTeamsAreThereInIpl);
+		
+		char startingLetterOfCaptainOfRcb='V';
+		System.out.println(startingLetterOfCaptainOfRcb);
+		
+		String favouriteTeam="Royal Challgers Bengalore";
+		System.out.println(favouriteTeam);
+	}
+}
