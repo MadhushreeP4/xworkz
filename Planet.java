ecdd9a879abaf6b14d5b6396cbe5bf4505623308
MadhushreeP4 <72905721+MadhushreeP4@users.noreply.github.com>
2020-10-19 20:02:16 +0530

    Eighth commit

---
+++ Planet.java
@@ -0,0 +1,31 @@
+public class Planet{
+	public static void main(String[] args)
+	{
+		byte planetsInOurSolarSystem=8;
+		System.out.println(planetsInOurSolarSystem);
+		
+		short numberOfMoonsInSolarSystem=200;
+		System.out.println(numberOfMoonsInSolarSystem);
+		
+		int activeDoctorsInIndia=926000;
+		System.out.println(activeDoctorsInIndia);
+		
+		long populationOfAmerica=328200000;
+		System.out.println(populationOfAmerica);
+		
+		float highestRecordOfRainFallInKarnataka=0.0101f;
+		System.out.println(highestRecordOfRainFallInKarnataka);
+		
+		double rootOfOneThousandNineHundred=43.588989435407;
+		System.out.println(rootOfOneThousandNineHundred);
+		
+		boolean alternativeNameOfMoonIsLuna=true;
+		System.out.println(alternativeNameOfMoonIsLuna);
+		
+		char startingLetterOfOurContinent='A';
+		System.out.println(startingLetterOfOurContinent);
+		
+		String ourState="Karnataka";
+		System.out.println(ourState);
+	}
+}
