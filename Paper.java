0623c227e8438a9b2c977fb16ce0059b6a34fb42
MadhushreeP4 <72905721+MadhushreeP4@users.noreply.github.com>
2020-10-19 19:57:17 +0530

    Fifth commit

---
+++ Paper.java
@@ -0,0 +1,32 @@
+public class Paper{
+	public static void main(String[] args)
+	{
+		byte fingers=20;
+		
+		short daysInAYear=365;
+		
+		int numberOfEngineersInIndia=1500000;
+		
+		long populationOfIndia=1380004385;
+		
+		float percentageOfStudent=80.83f;
+		
+		double massOfElectron=0.00054859;
+		
+		boolean earthIsFlat=false;
+		
+		char grade='a';
+		
+		String name="madhushree";
+		
+		System.out.println(fingers);
+		System.out.println(daysInAYear);
+		System.out.println(numberOfEngineersInIndia);
+		System.out.println(populationOfIndia);
+		System.out.println(percentageOfStudent);
+		System.out.println(massOfElectron);
+		System.out.println(earthIsFlat);
+		System.out.println(grade);
+		System.out.println(name);
+	}
+}
