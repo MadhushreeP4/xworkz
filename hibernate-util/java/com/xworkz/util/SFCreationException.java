25a60feefd11da2e404a804f430274010cb204d5
MadhushreeP4 <72905721+MadhushreeP4@users.noreply.github.com>
2021-03-08 19:20:59 +0530

    Add files via upload

---
+++ hibernate-util/java/com/xworkz/util/SFCreationException.java
@@ -0,0 +1,13 @@
+package com.xworkz.util;
+
+public class SFCreationException extends RuntimeException{
+	
+	public SFCreationException(String msg) {
+		super(msg);
+	}
+	
+	public SFCreationException() {
+		
+	}
+
+}
